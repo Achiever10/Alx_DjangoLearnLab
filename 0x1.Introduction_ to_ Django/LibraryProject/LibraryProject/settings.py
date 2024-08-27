0d72643e3ce9b52a328421ddc357298577bee9a3
Abisagboola Beloved <Abisagboolabeloved@gmail.com>
2024-08-27 21:37:54 +0100

    Implementing and Interacting with Django Models

--- 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    'bookshelf.apps.BookshelfConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
